42bd1896b24a58ddc7698428547f601086c4a8fa
Yumiko Tezuka <ytezuka5@gmail.com>
2016-11-19 03:24:37 +0000

    test app

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+ def index
+
+    client = Instagram.client(access_token: "1437067778.76764d5.7181aed7ea1c41e68e1fc3077b939684")
+
+    @medias = client.user_recent_media
+
+  end
+
+end  
+
+
+
---
+++ config/initializers/instagram.rb
@@ -0,0 +1,7 @@
+
+require "instagram"
+    Instagram.configure do |config|
+    config.client_id = ENV["76764d542cd0455998280841bf855238"]
+    config.client_secret =ENV["2907064c4e794f23884b4bdab3c9eaa5"]
+    
+end
